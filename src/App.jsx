0e7ef8113d5db11182a61f9f100034f045360ae8
shubh-37 <shubh.arya37@gmail.com>
2023-04-29 20:57:46 +0530

    added routes to pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,22 @@
-import React, { useState } from 'react'
-
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
 import './App.css'
+import CreateEmp from './pages/CreateEmp';
+import CreateOrg from './pages/CreateOrg';
+import LandingPage from './pages/LandingPage';
+import SignUp from './pages/SignUp';
 
 function App() {
 
 
   return (
    <React.Fragment>
-
+      <Routes>
+        <Route path='/' element={<LandingPage />} />
+        <Route path='/createEmp' element={<CreateEmp />} />
+        <Route path='/createOrg' element={<CreateOrg />} />
+        <Route path='/signUp' element ={<SignUp />} />
+      </Routes>
    </React.Fragment>
   )
 }
